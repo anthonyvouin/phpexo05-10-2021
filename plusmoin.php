6f717134523b141f73875c16c84707cd63e547a5
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2021-10-05 13:39:24 +0100

    Update plusmoin.php

--- plusmoin.php
+++ plusmoin.php
@@ -1,4 +1,45 @@
 <?php 
-$number=rand(0,10);
 
-readline("Rentre un nombre");
+
+$number = rand(0, 100);
+
+
+
+function devineMonChiffre($vie, $number)
+{
+    $keypress = -1;
+    
+    while ( $vie > -1 && $keypress !== $number)
+    {
+        
+        $keypress = readline("Merci de rentrer une valeur");
+        if($vie > -1) {
+        if ($keypress < $number )
+        {
+            echo'cest plus '.PHP_EOL;
+            echo"Tu as $vie vie".PHP_EOL;
+            $vie--;
+
+        }
+        
+        else if ( $keypress > $number )
+        {
+            echo'cest moin '.PHP_EOL;
+            echo"Tu as $vie vie" .PHP_EOL;
+            $vie--;
+        }
+        
+        if ( $keypress == $number )
+        {
+            echo'Vous avez gagné ';
+            die;
+        }
+      }
+
+    }
+    echo "Tes mauvais Jack";
+}
+
+
+
+   devineMonChiffre($argv[1], $number);
