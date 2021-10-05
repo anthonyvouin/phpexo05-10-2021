2c3ad057842d621782c606f742b1883b4d803383
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2021-10-05 15:15:29 +0100

    Update retourneur.php

--- retourneur.php
+++ retourneur.php
@@ -1,13 +1,20 @@
 <?php
 
-function retourneMonMot($mot){
-$list = [];
-$mot = explode(" ",$mot);
+function retourneur($mot)
+{
 
-foreach($mot as $value){
-$value = strrev($value);
-array_push($list, $value);
-  }
-  $mot = implode(" ", $list);
-  echo $mot;
+    $list = [];
+    $mot = explode(" ", $mot);
+
+    foreach($mot as $value)
+    {
+        $value = strrev($value);
+        array_push($list, $value);
+    }
+        
+    $mot = implode(" ", $list);
+    echo $mot;
+    
 }
+
+retourneur($argv[1]);
