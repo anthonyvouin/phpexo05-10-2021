226e400e42c40cc32a899409ef123905546bba68
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2021-10-09 19:01:59 +0100

    Update ranger.php

--- ranger.php
+++ ranger.php
@@ -1,22 +1,19 @@
 <?php
-// @formatter:on
-// Je récupère les données dans les arguments
+
 $arguments = $argv;
-// je retire le premeire element de mon tbaleau
+
 array_shift($arguments);
 
 
-// Je crée un tableau
 $requete = array();
-// Je boucle sur le tableau
+
 foreach ($arguments as $argument)
 sort($arguments);
 
-// Je push mes données dans mon tableau
+
 
 array_push($requete, $argument);
 
 
-// jenvoi les infos à mon json
 $json = json_encode($arguments);
 $bytes = file_put_contents("myfile.json", $json); 
