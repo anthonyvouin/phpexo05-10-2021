b6655502f93267550ef7d8c78ac54b31e3015348
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2021-10-23 14:59:31 +0100

    n
    b

--- rechercheBinaire.php
+++ rechercheBinaire.php
@@ -1,14 +1,53 @@
 <?php
 
-
-$json = file_get_contents("annuaire.json");
-
+$json = file_get_contents("data.json");
 $parsed_json = json_decode($json);
+// var_dump($parsed_json[0]);
 
 
 
-var_dump(json_decode($json));
+$val="";
+
+function binarySearch($argv, $parsed_json)
+{
+    $min = 0;
+    $max = 68;
+    $milieu = ($min+$max)/2;
+    $tableau = $parsed_json;
+    $step = 0;
 
   
-?>
 
+    while($argv != $parsed_json[$milieu]->{'name'} && $min<$max ){
+
+        if($argv < $parsed_json[$milieu]->{'name'}){
+            $max = $milieu - 1;
+        }else{
+            $min = $milieu +1 ;
+        }
+        $milieu = ($min + $max)/2;
+
+    }
+
+    if($argv == $parsed_json[$milieu]->{'name'}){
+        var_dump($parsed_json[$milieu]->{'num'});
+    }else{
+        echo"Tu as perdu";
+    }
+   
+
+
+
+
+
+    //var_dump($max);
+  // var_dump($parsed_json[1] -> {'name'});
+}
+
+// $a = "a";
+// $b = "b";
+// if($a<$b){
+//     echo 'true';
+// }
+
+binarySearch($argv, $parsed_json);
